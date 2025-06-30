ec3e51b4c11fe7b24e00283a00a6d1cceb3985c5
Mireia <mireia95.granzotto@gmail.com>
2025-07-01 00:38:32 +0200

    URL for picture detail

--- src/Components/PictureDetail/PictureDetails.jsx
+++ src/Components/PictureDetail/PictureDetails.jsx
@@ -3,26 +3,31 @@ import './PictureDetails.css';
 import { useParams } from 'react-router-dom';
 
 const PictureDetails = () => {
-  const [picture, setPicture] = useState('');
+  const [picture, setPicture] = useState();
   const { id } = useParams();
   console.log(id);
   useEffect(() => {
-    fetch(`https://images-api.nasa.gov/asset/${id}`)
+    fetch(`https://images-api.nasa.gov/search?nasa_id=${id}`)
       .then((res) => res.json())
       .then((res) => {
-        console.log(res);
-        setPicture(res);
+        setPicture(res.collection.items[0]);
       });
   }, []);
 
+  console.log(picture);
+
   return (
     <section id='pictureDetail'>
-      <h3> PRUEBA </h3>
-      <p> </p>
-      <p className='date'></p>
-      <div>
-        <img></img>
-      </div>
+      {picture && (
+        <>
+          <h3> {picture.data[0].title} </h3>
+          <p>{picture.data[0].description_508} </p>
+          <p className='date'>{picture.data[0].date_created}</p>
+          <div>
+            <img src={picture.links[2].href} alt='test'></img>
+          </div>
+        </>
+      )}
     </section>
   );
 };

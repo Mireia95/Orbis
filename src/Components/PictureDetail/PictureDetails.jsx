1d7a08ee7b98b78f06fc88f1ca709f466ac42e21
Mireia <mireia95.granzotto@gmail.com>
2025-07-02 14:46:29 +0200

    udpate pictureDetail

--- src/Components/PictureDetail/PictureDetails.jsx
+++ src/Components/PictureDetail/PictureDetails.jsx
@@ -1,35 +1,33 @@
-import { useEffect, useState } from 'react';
-import './PictureDetails.css';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react'
+import './PictureDetails.css'
+import { useParams } from 'react-router-dom'
 
 const PictureDetails = () => {
-  const [picture, setPicture] = useState();
-  const { id } = useParams();
-  console.log(id);
+  const [picture, setPicture] = useState()
+  const { id } = useParams()
+
   useEffect(() => {
     fetch(`https://images-api.nasa.gov/search?nasa_id=${id}`)
       .then((res) => res.json())
       .then((res) => {
-        setPicture(res.collection.items[0]);
-      });
-  }, []);
-
-  console.log(picture);
+        setPicture(res.collection.items[0])
+      })
+  }, [])
 
   return (
-    <section id='pictureDetail'>
+    <section id='pictureDetail' className='flex'>
       {picture && (
         <>
           <h3> {picture.data[0].title} </h3>
           <p>{picture.data[0].description_508} </p>
-          <p className='date'>{picture.data[0].date_created}</p>
-          <div>
+          <p className='date'>{picture.data[0].date_created.slice(0, 10)}</p>
+          <div className='imgDetail'>
             <img src={picture.links[2].href} alt='test'></img>
           </div>
         </>
       )}
     </section>
-  );
-};
+  )
+}
 
-export default PictureDetails;
+export default PictureDetails
